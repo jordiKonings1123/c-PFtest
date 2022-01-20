582e96c80025acd674e05bfc8aa4093987b10d04
Jordi Konings <jordi.konings@vdabcampus.be>
2022-01-20 14:00:47 +0100

    Add project files.

---
+++ cFPTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cFPTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
